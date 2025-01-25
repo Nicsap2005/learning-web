54b14d633f46514c5e20e96751315e2accc4749d
Nicsap2005 <71231021@students.ukdw.ac.id>
2025-01-25 09:24:38 +0700

    Add files via upload

---
+++ atm/login_state.php
@@ -0,0 +1,56 @@
+<?php
+    $servername = "localhost";
+    $username = "root";  // Your MySQL username
+    $password = "";  // Your MySQL password
+    $dbname = "client_bank";  // Your database name
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("cant connect to the database". $conn->connect_error);
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = $_POST["nama_input"];
+        $rekening = $_POST["rekening_input"];
+        $pin = $_POST["pin_input"];
+        // $sql = "SELECT * FROM client_info WHERE nama = `$username` AND rekening = `$rekening` and pin = `$pin`";
+        $sql = "SELECT * FROM client_info WHERE nama = '$username' AND rekening = '$rekening' AND pin ='$pin'";
+
+
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            echo "client found!";
+            header("Location: option_state.php");
+            exit;
+        }
+        else{
+            echo "client not found!";
+        }
+    }
+    
+    $conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label>masukan nama anda: </label>
+        <input name = "nama_input" type = "text">
+        <br>
+        <label>masukan rekening anda: </label>
+        <input name = "rekening_input" type = "text">        
+        <br>
+        <label>Masukan PIN anda:</label>
+        <input name = "pin_input" type = "password" maxlength="6" pattern="\d{6}">
+        <button type="submit">Enter</button>
+    </form>
+
+</body>
+</html>
---
+++ atm/option_state.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>option page</h1>
+    <button onclick="location.href='tarik_tunai.php'">tarik tunai</button>
+    <button onclick="location.href='cek_saldo.php'">cek saldo</button>
+    
+</body>
+</html>
